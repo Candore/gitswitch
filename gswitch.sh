9b0c3c051626511cbe9ab441bbe6f5916345ff2c
Candore <v.k.kavin@gmail.com>
2019-11-08 13:22:02 -0500

    Updating add_id_rsa
    with correct logic to find and add rsa file

--- gswitch.sh
+++ gswitch.sh
@@ -6,18 +6,17 @@ function gswitch() {
     git config --global user.email $(git config --global secondary.email)
     git config --global secondary.name $tempname
     git config --global secondary.email $tempemail
-
+     
     add_rsa_id_to_ssh $(git config --global user.name)
 }
 
-function add_rsa_id_to_ssh() {
-    val=$(echo $1 | tr '[A-Z]' '[a-z]') ## lowercasing input parameter
-    if [ $(find ~/.ssh -name \*"$val") ]; then ## pattern matching file name with the given input to see if it exisits in ssh
-        file= find ~/.ssh -name \*"$val" -exec basename {} \; ## returns only the base name instead of the full path +name
-        echo $file
-        cd ~/.ssh/ && ssh-add -D && ssh-add $file  # delete all keys in the agent and add new
+function add_rsa_id_to_ssh() {    
+    if [ $(find ~/.ssh -iname \*"$1") ]; then ## pattern matching file name - case insensitive,  with the given input to see if it exisits in ssh       
+        echo "file found: $(echo $(find ~/.ssh -iname \*"$1" -execdir echo '{}' ';');)" 
+        cd ~/.ssh/ && ssh-add -D && ssh-add $(echo $(find ~/.ssh -iname \*"$1" -execdir echo '{}' ';');) # delete all keys in the agent and add new
     else #file does not exist
         cd ~/.ssh/ && ssh-add -D && ssh-add id_rsa #add default ssh id if a secondary is not found.
     fi
 }
+
 gswitch
