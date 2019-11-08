3f9ba72eecb02d4915b3e794fb35799e179d77dc
Candore <v.k.kavin@gmail.com>
2019-11-07 19:31:56 -0500

    Adding add_rsa_id_to_ssh method

--- gswitch.sh
+++ gswitch.sh
@@ -1,13 +1,22 @@
-function gswitch {
-    
-    tempname=$(git config --global user.name) 
+function gswitch() {
+
+    tempname=$(git config --global user.name)
     tempemail=$(git config --global user.email)
     git config --global user.name $(git config --global secondary.name)
     git config --global user.email $(git config --global secondary.email)
     git config --global secondary.name $tempname
     git config --global secondary.email $tempemail
 
-    # cat ~/.gitconfig
+    add_rsa_id_to_ssh $(git config --global user.name)
 }
 
+function add_rsa_id_to_ssh() {
+    val=$(echo $1 | tr '[A-Z]' '[a-z]') ## lowercasing input parameter
+    if [ $(find ~/.ssh -name \*"$val") ]; then ## pattern matching file name with the given input to see if it exisits in ssh
+        file= find ~/.ssh -name \*"$val" -exec basename {} \; ## returns only the base name instead of the full path +name
+        cd ~/.ssh/ && ssh-add $file
+    else #file does not exist
+        cd ~/.ssh/ && ssh-add id_rsa #add default ssh id if a secondary is not found.
+    fi
+}
 gswitch
