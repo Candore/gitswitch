755d58d98f91793e95f8da88a0bb633186950fe2
Candore <v.k.kavin@gmail.com>
2019-11-11 11:07:44 -0500

    updating init and switch

--- init.sh
+++ init.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 source gswitch.sh #Importing file
 source user.sh    #Importing file
+source colors.sh
 
 function set_secondary() {
     _set_username
     _set_useremail
+    # https://help.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent    
+    cd ~/.ssh/ && ssh-keygen -f $(git config --global secondary.name) -t ecdsa -b 521 
+    
+    echo "add generated key to ssh ssh-add id_rsa$(git config --global secondary.name)"
+    echo "copy public key to github ssh settings"
 }
 
 function init() {
